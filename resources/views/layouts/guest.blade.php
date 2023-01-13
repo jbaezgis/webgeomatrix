fa48528ac0d6cd077be7dbbcf6b07a85ba51833b
Yoel Baez <jbaezgis@gmail.com>
2023-01-13 11:59:41 -0400

    footer fixed - 1.0.0.4

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -69,7 +69,7 @@
 
         
         <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 border-t">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ now()->year }} Geomatrix - {{ __('All Rights Reserved') }}.
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ now()->year }} Geomatrix.
             </span>
             <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
