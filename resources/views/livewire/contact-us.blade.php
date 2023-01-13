e8e01342bc14320a802f9a183f2d405fbcd5ffed
Yoel Baez <jbaezgis@gmail.com>
2023-01-13 15:58:40 -0400

    contact-us info - 1.0.0.10

--- resources/views/livewire/contact-us.blade.php
+++ resources/views/livewire/contact-us.blade.php
@@ -7,13 +7,14 @@
         </div>
     </div>
 
-    <div class="max-w-7xl mx-auto pt-10 space-y-6">
-        {{-- <p>Firma experta en el diagnóstico, diseño, preparación y evaluación de proyectos de recuperación, manejo, protección y aprovechamiento sostenible de recursos naturales renovables y no reno.  En los sectores de riego, planeamiento físico, minería, líneas de transmisión eléctrica, generación hidroeléctrica y ecoturismo, hemos trabajado en todas las etapas de proyectos, en los aspectos ambiéntales, socioeconómicos, organizacionales e institucionales.</p> 
-
-        <p>GEOMATRIX ha desarrollado ventajas competitivas en la ejecución de proyectos de desarrollo sostenible, gestión de procesos de articulación minería - comunidades.  Firma especializada en geomática con experiencia comprobada en el diseño e implementación de soluciones funcionales, ágiles, automatizadas y adaptables.</p> 
-        
-        <p>Habilidad para la investigación y transferencia tecnológica con experiencia en la ejecución de estudios básicos para elaboración de programas de mitigación de impactos negativos en el ambiente.  Actualizado en los temas de sostenibilidad, género y participación comunitaria. </p> 
-         
-        <p>Experiencia en el ámbito institucional público, privado y no lucrativo relacionado con la gestión de servicios, y documentación de recursos.  Consultora ambiental.  Habilidad para la comunicación verbal y escrita.  Coordinadora de equipos multidisciplinarios e interinstitucionales. </p>  --}}
+    <div class="max-w-7xl mx-auto pt-10 space-y-4">
+        <div class="text-center">
+            <div class="text-lg text-gray-600">Correo:</div>
+            <div class="text-2xl text-green-500">info@geomatrixgts.com</div>
+        </div>
+        <div class="text-center">
+            <div class="text-lg text-gray-600">Teléfono</div>
+            <div class="text-2xl text-green-500">809-908-1136</div>
+        </div>
     </div>
 </div>
