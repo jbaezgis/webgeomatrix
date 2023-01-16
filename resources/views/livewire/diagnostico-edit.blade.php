d5fd22352be2f96a6c719aa1949248a4c5126666
Yoel Baez <jbaezgis@gmail.com>
2023-01-16 18:06:45 -0400

    diagnosticos fixed- 1.0.0.21

--- resources/views/livewire/diagnostico-edit.blade.php
+++ resources/views/livewire/diagnostico-edit.blade.php
@@ -68,13 +68,16 @@
                         </div>
                     </div>
 
-                    <div class="flex items-center gap-4">
-                        <div class="border p-2 mb-4 rounded">
-                            <input type="file" wire:model="url">
+                    <div class="flex gap-2">
+                        <div class="mb-2">
+                            {{-- <input type="file" wire:model="url"> --}}
+                            {{-- <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Upload file</label> --}}
+                            <input wire:model="url" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="image_help" type="file">
+                            <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="image_help">No soporta imagenes mayores de 2MB</div>
                             <x-jet-input-error for="url"/>
                         </div>
                         <div>
-                            <button wire:click="createImage" wire:loading.attr="disabled" wire:target="createImage, url" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Agregar</button>
+                            <button wire:click="createImage" wire:loading.attr="disabled" wire:target="createImage, url" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Agregar</button>
                         </div>
                     </div>
 
