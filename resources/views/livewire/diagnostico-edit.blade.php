70aefa631eade8c3e80b362446c958086701d888
Yoel Baez <jbaezgis@gmail.com>
2023-01-16 16:37:32 -0400

    modulo diagnosticos fixed - 1.0.0.15

--- resources/views/livewire/diagnostico-edit.blade.php
+++ resources/views/livewire/diagnostico-edit.blade.php
@@ -252,13 +252,13 @@
                                     <td class="px-6 py-4">
                                         <div class="flex gap-2 items-center">
                                            
-                                            <button class="text-blue-500" wire:click="updateFlujoShowModal({{ $item->id }})">
+                                            <button class="text-blue-500" wire:click="updateCaracteristicaShowModal({{ $item->id }})">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                                   </svg>   
                                             </button>
                                            
-                                            <button class="text-red-500" wire:click="deleteFlujoShowModal({{ $item->id }})">
+                                            <button class="text-red-500" wire:click="deleteCaracteristicaShowModal({{ $item->id }})">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                                 </svg>
@@ -327,13 +327,13 @@
                                     <td class="px-6 py-4">
                                         <div class="flex gap-2 items-center">
                                            
-                                            <button class="text-blue-500" wire:click="updateFlujoShowModal({{ $item->id }})">
+                                            <button class="text-blue-500" wire:click="updateDatoShowModal({{ $item->id }})">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                                   </svg>   
                                             </button>
                                            
-                                            <button class="text-red-500" wire:click="deleteFlujoShowModal({{ $item->id }})">
+                                            <button class="text-red-500" wire:click="deleteDatoShowModal({{ $item->id }})">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                                 </svg>
