59ddf1060ad81fe68f93d5020c285b94b5b4f101
Yoel Baez <jbaezgis@gmail.com>
2023-01-23 10:27:06 -0400

    diagnosticos mobile view - 1.0.0.30

--- resources/views/livewire/diagnosticos.blade.php
+++ resources/views/livewire/diagnosticos.blade.php
@@ -98,7 +98,7 @@
 
             <div class="mt-2">
                 @foreach ($diagnosticos as $item)
-                    <div class="border bg-white shadow">
+                    <div class="border bg-white shadow mb-4">
                         <div class="flex gap-4 px-4 pt-4">
                             <div>
                                 <div class="uppercase text-gray-400 text-xs">Id</div>
@@ -115,39 +115,46 @@
                             </div>
                         </div>
 
-                        <div class="flex gap-4 mt-4 px-4 pb-4">
-                            <div>
+                        <div class="flex gap-4 mt-4 px-4 pb-4 ">
+                            <div class="w-full">
                                 <div class="uppercase text-gray-400 text-xs">Responsable</div>
-                                <div class="text-gray-700">
-                                    {{ $item->responsable }}
+                                <div class="text-gray-700 truncate">
+                                    <span>{{ $item->responsable }}</span>
                                 </div>
                             </div>
-                            <div>
-                                <div class="uppercase text-gray-400 text-xs">Fecha</div>
-                                <div class="text-gray-700">
-                                    {{ $item->fecha }}
-                                </div>
-                            </div>
+                            
+                           
                         </div>
 
-                        <div class="flex gap-4 items-center bg-gray-100 justify-end p-2">
-                            <a href="{{ url('admin/diagnostico/view/'.$item->id) }}" class="font-medium text-gray-600 dark:text-gray-500 hover:underline">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                  </svg>                                      
-                            </a>
-                            <a href="{{ url('admin/diagnostico/edit/'.$item->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                  </svg>                                      
-                            </a>
-                           
-                            <button class="text-red-500" wire:click="deleteShowModal({{ $item->id }})">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                </svg>
-                            </button>
+                        <div class=" bg-gray-100  p-2">
+                            <div class="flex justify-between">
+                                <div class="flex gap-2 items-center">
+                                    <div class=" text-gray-400 text-sm">Fecha:</div>
+                                    <div class="text-gray-500 text-sm">
+                                        {{ $item->fecha }}
+                                    </div>
+                                </div>
+                                <div class="flex gap-4 items-center justify-end">
+                                    <a href="{{ url('admin/diagnostico/view/'.$item->id) }}" class="font-medium text-gray-600 dark:text-gray-500 hover:underline">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                          </svg>                                      
+                                    </a>
+                                    <a href="{{ url('admin/diagnostico/edit/'.$item->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                          </svg>                                      
+                                    </a>
+                                   
+                                    <button class="text-red-500" wire:click="deleteShowModal({{ $item->id }})">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                            
                         </div>
                     </div>
                 @endforeach
