3eda327af95564909eff26cceaba7b7969964f58
Yoel Baez <jbaezgis@gmail.com>
2023-01-24 12:33:59 -0400

    diagnosticos mobile view - 1.0.0.33

--- resources/views/livewire/diagnostico-edit.blade.php
+++ resources/views/livewire/diagnostico-edit.blade.php
@@ -296,8 +296,8 @@
             <div class="border-b sm:hidden">
                 @foreach ($flujos as $item)
                    <div class="flex bg-white p-2 shadow-lg mb-2 items-center border-l-2 border-l-blue-500 mt-4">
-                       <div class="font-medium w-80">
-                           <div class="w-full truncate">
+                       <div class="font-medium">
+                           <div class="w-60 truncate">
                                {{ $item->descripcion }}
                            </div>
                            <div class="text-sm text-gray-500">
@@ -308,7 +308,7 @@
                                @endif
                            </div>
                        </div>
-                       <div class="basis-1/4 text-end">
+                       <div class="w-40 text-end">
                            <button class="text-blue-500 inline-flex border border-blue-500 rounded px-2 py-0.1 hover:bg-blue-50 focus:ring-2 focus:ring-blue-100" wire:click="updateFlujoShowModal({{ $item->id }})"> 
                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
