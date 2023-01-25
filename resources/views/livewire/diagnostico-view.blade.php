5125fb5df348d814a7ac6fc54e17ef321749b76f
Yoel Baez <jbaezgis@gmail.com>
2023-01-24 21:36:44 -0400

    diagnosticos mobile view and styles - 1.0.0.36

--- resources/views/livewire/diagnostico-view.blade.php
+++ resources/views/livewire/diagnostico-view.blade.php
@@ -152,7 +152,7 @@
             <div class="">{{ $diagnostico->observaciones_generales }}</div>
         </div>
 
-        <div class="px-2 mt-6 border-t pt-4">
+        <div class="px-2 mt-6 border-t pt-4 mb-8">
             <div class="mb-4">
                 <div class="text-gray-900 text-lg font-bold text-center">
                     RECURSOS DISPONIBLES
@@ -162,59 +162,71 @@
             <div>
                 <ol class="list-decimal pl-4 space-y-4">
                     @foreach ($recursos as $item)
-                    <li class="font-medium">{{ $item->descripcion }}</li>
-                    <ul class="list-disc list-outside pl-4">
-                        <div class="relative overflow-x-auto">
-                                        
-                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                                    <tr class="border">
-                                        <th scope="col" class="px-3 py-1">
-                                            Entrada / Proveedor
-                                        </th>
-                                        <th scope="col" class="px-3 py-1">
-                                            Tratamiento
-                                        </th>
-                                        <th scope="col" class="px-3 py-1">
-                                            Salida / Cliente
-                                        </th>
-                                        <th scope="col" class="px-3 py-1">
-                                            SIG
-                                        </th>
-                                        <th scope="col" class="px-3 py-1">
-                                            Comentario
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($caracteristicas->where('flujo_id', $item->id) as $item)
-                                        <tr class="bg-white border dark:border-gray-700 hover:bg-gray-50">
-                                        
-                                            <th scope="row" class="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                {{ $item->entrada_proveedor }}
-                                            </th>
-                                            <th scope="row" class="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                {{ $item->tratamiento }}
-                                            </th>
-                                            <th scope="row" class="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                {{ $item->salida_cliente }}
-                                            </th>
-                                            <th scope="row" class="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                @if ($item->sig == true)
-                                                    Si
-                                                @else
-                                                @endif
-                                            </th>
-                                            <th scope="row" class="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                {{ $item->comentario }}
-                                            </th>
-                                        </tr>
-                                    @endforeach
-                                    
-                                </tbody>
-                            </table>
-                        </div>
-                    </ul>
+                    <li class="font-medium">{{ $item->equipo }} {{ $item->marca_modelo }} 
+                        <span class="text-sm text-gray-600"> -
+                            {{ $item->capacidades ? '' . $item->capacidades . ',':'' }} {{ $item->red_servidor == true ? 'acceso a la red' : ''  }} {{ $item->acceso_internet == true ? ', acceso a internet' : ''  }}, Asignado a: {{ $item->personal_asignado }}
+                        </span>
+                    </li>
+                        @if ($softwares->where('recurso_id', $item->id)->count())
+                            <ul class="list-disc list-outside pl-4">
+                                <div class="relative overflow-x-auto">
+                                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                                            <tr class="border">
+                                                <th scope="col" class="px-3 py-1">
+                                                    Aplicacion
+                                                </th>
+                                                <th scope="col" class="px-3 py-1">
+                                                    Desarrollador
+                                                </th>
+                                                <th scope="col" class="px-3 py-1">
+                                                    Tipo de Licencia
+                                                </th>
+                                                <th scope="col" class="px-3 py-1">
+                                                    Uso
+                                                </th>
+                                                <th scope="col" class="px-3 py-1">
+                                                    Frecuencia de Uso
+                                                </th>
+                                                <th scope="col" class="px-3 py-1">
+                                                    Personal Asignado
+                                                </th>
+                                                <th scope="col" class="px-3 py-1">
+                                                    
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($softwares->where('recurso_id', $item->id) as $item)
+                                                <tr class="bg-white border dark:border-gray-700 hover:bg-gray-50">
+                                                
+                                                    <th scope="row" class="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                        {{ $item->aplicacion }}
+                                                    </th>
+                                                    <th scope="row" class="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                        {{ $item->desarrollador }}
+                                                    </th>
+                                                    <th scope="row" class="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                        {{ $item->tipo_licencia }}
+                                                    </th>
+                                                    <th scope="row" class="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                        {{ $item->uso }}
+                                                    </th>
+                                                    <th scope="row" class="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                        {{ $item->frecuencia_uso }}
+                                                    </th>
+                                                    <th scope="row" class="px-2 py-1 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                        {{ $item->personal_asignado }}
+                                                    </th>
+                                                
+                                                </tr>
+                                            @endforeach
+                                            
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </ul>
+                        @endif
                     @endforeach
                 </ol>
             </div>
