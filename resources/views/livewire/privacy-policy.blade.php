47b789c6c16fda116c641fd937444e2c2db5088f
Yoel Baez <jbaezgis@gmail.com>
2023-02-08 21:21:08 -0400

    privacy text - 1.0.0.40

--- resources/views/livewire/privacy-policy.blade.php
+++ resources/views/livewire/privacy-policy.blade.php
@@ -5,4 +5,17 @@
             <div></div>
         </div>
     </div>
+
+    <div class="max-w-7xl mx-auto pt-10 space-y-6 mb-2">
+        <p>El usuario de Internet que accede al sitio web geomatrixgts.com lo hace de forma anónima y no está registrado por nosotros para 
+            ningún servicio en línea. El usuario permanece anónimo durante su búsqueda a través de toda la información en el sitio web, 
+            y en ningún momento se registran sus datos personales para ningún servicio en línea.</p> 
+
+        <p>Sin embargo, utilizamos identificadores de usuario (cookies) en nuestros sitios web para recopilar información sobre el uso 
+            del sitio web, como el servidor al que está conectada la computadora del usuario, el tipo de navegador 
+            (por ejemplo, Firefox o Internet Explorer) y cómo se convirtió en cliente potencial Geomatrix. 
+            Utilizamos dicha información únicamente para ayudarnos a mejorar nuestras políticas de marketing, y los datos personales del 
+            usuario de Internet no se incluyen en la captura de datos.</p> 
+        
+    </div>
 </div>
